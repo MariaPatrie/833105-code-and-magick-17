b81f586379656f4b874b5c806157be5e87536350
MariaPatrie <mariapatrie@gmail.com>
2019-06-10 16:34:28 +0300

    доработка

--- js/stat.js
+++ js/stat.js
@@ -17,32 +17,17 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
-};
-
-window.renderStatistics = function (ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
+var renderText = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
   ctx.fillText('Ура вы победили!', CLOUD_X + GAP_20, CLOUD_Y + GAP_20);
   ctx.fillText('Список результатов:', CLOUD_X + GAP_20, CLOUD_Y + GAP_20 + TEXT_HEIGHT);
+};
 
-  var maxTime = getMaxElement(times);
-  var colorOpacity = Math.random();
+var renderColumns = function (ctx, players, times) {
+    var colorOpacity = Math.random();
 
-  for (var i = 0; i < players.length; i++) {
     ctx.fillStyle = '#000';
     ctx.fillText(
         times[i].toFixed(),
@@ -63,5 +48,30 @@ window.renderStatistics = function (ctx, players, times) {
         BAR_WIDTH,
         (barHeight * times[i]) / maxTime
     );
+  }
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+
+  renderText(ctx);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < players.length; i++) {
+    renderColumns(ctx, players, times);
   }
 };
